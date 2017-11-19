fdb7ba9866b02325ca87b217e59558e9f340c254
godkim12 <sakkarinsuraban@gmail.com>
2017-11-19 15:21:25 +0700

    Edit information

--- src/oop_lab/s359211110083/rmutsv/com/HelloWold.java
+++ src/oop_lab/s359211110083/rmutsv/com/HelloWold.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
 
     System.out.println("Hello World, I'am KIM");
     System.out.println("sakkarin suraban");
-    System.out.println("Information system");
-    System.out.println("MT:RMUTSV");
+    System.out.println("Information system,MT:RMUTSV");
+
 
 }//main
 
