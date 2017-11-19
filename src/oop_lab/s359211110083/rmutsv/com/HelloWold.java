e3e7c20fd9d60d0c206083ea0f7290210826ad82
godkim12 <sakkarinsuraban@gmail.com>
2017-11-19 15:18:45 +0700

    Add information

--- src/oop_lab/s359211110083/rmutsv/com/HelloWold.java
+++ src/oop_lab/s359211110083/rmutsv/com/HelloWold.java
@@ -6,6 +6,9 @@ public class HelloWold {
 public static void main(String[] args) {
 
     System.out.println("Hello World, I'am KIM");
+    System.out.println("sakkarin suraban");
+    System.out.println("Information system");
+    System.out.println("MT:RMUTSV");
 
 }//main
 
