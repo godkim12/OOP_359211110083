1961de429d4ffeecfa198557a1c4b8445618093c
godkim12 <sakkarinsuraban@gmail.com>
2017-11-19 14:48:57 +0700

    Testing upload to github

---
+++ src/oop_lab/s359211110083/rmutsv/com/HelloWold.java
@@ -0,0 +1,13 @@
+package oop_lab.s359211110083.rmutsv.com;
+
+public class HelloWold {
+
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I'am KIM");
+
+}//main
+
+
+}//class
