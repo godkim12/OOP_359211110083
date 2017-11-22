731dcd60dbb946116722cf0d01e10f620ebf954f
godkim12 <sakkarinsuraban@gmail.com>
2017-11-22 11:35:50 +0700

    TestInputEdit

--- src/ooplab2/TestInput.java
+++ src/ooplab2/TestInput.java
@@ -14,18 +14,18 @@ public class TestInput {
         //Input Data by using BufferedReader
         System.out.println("what is your name?: ");
         String name = reader.readLine();
-        System.out.println("Your name is" + name);
+        System.out.println("Your name is "+name);
         System.out.print("How old are you?: ");
         int age = Integer.parseInt(reader.readLine());
-        System.out.println("You are " + age + " years old.");
+        System.out.println("You are "+age+" years old.");
 
         //Scanner
         Scanner scanner = new Scanner(System.in);
         System.out.print("Where are you from?: ");
         String address = scanner.nextLine();
-        System.out.println("You came form" + address);
+        System.out.println("You came form "+address);
         System.out.print("what is your weight?: ");
         double weight = scanner.nextDouble();
-        System.out.print("Your wight: " + weight);
+        System.out.print("Your wight: "+weight);
     }//main
 }//class
