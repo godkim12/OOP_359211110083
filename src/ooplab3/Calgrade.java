94f0468115157750ef2844c308377b1b805aeeda
LAB_COM-MT_1 <sakkarinsuraban@gmail.com>
2017-12-06 09:38:22 +0700

    TestWhileLoop

--- src/ooplab3/Calgrade.java
+++ src/ooplab3/Calgrade.java
@@ -7,30 +7,32 @@
 package ooplab3;
 
 import java.util.Scanner;
+
 public class Calgrade {
-    public static void main(String [] args) {
-        Scanner scanner = new Scanner(System.in);
-        int score;
-        int count = 0;
-        for (int i=1; ) {
+   public static void main(String [] args) {
+       Scanner scanner = new Scanner(System.in);
+       int score;
+       int count = 0;
+       for (int i = 1; i <= 3; i++) {
+       System.out.print("Enter your score (0-100): ");
+       score = scanner.nextInt();
+       if (score < 0 || score > 100) {
+           count++;
+           if (count == 3) {
+               System.out.println("System Error.");
+               break;
+           }
+       }
+       else {
+           // test condition score
+           if (score < 49) System.out.println("Your grade is F");
+           else if (score < 59) System.out.println("your grade is D");
+           else if (score < 69) System.out.println("your grade is C");
+           else if (score < 79) System.out.println("your grade is B");
+           else System.out.println("your grade is A");
+           break;
+       }
+   }//for
 
-        }//for
-        System.out.print("Enter your score: ");
-        score = scanner.nextInt();
-
-
-
-        // test condition score
-    if (score<49)System.out.println("Your grade is F");
-        else if (score<59)System.out.println("your grade is D");
-            else if (score<69)System.out.println("your grade is C");
-                else if (score<79)System.out.println("your grade is B");
-                    else System.out.println("your grade is A");
-
-
-
-
-    }//main
-
-
+}//main
 }//class
