fc44e4e6a8ec0b8135cacc63156ed193a7a09502
LAB_COM-MT_1 <sakkarinsuraban@gmail.com>
2017-12-20 11:13:53 +0700

    myString

--- src/ooplab6/myString.java
+++ src/ooplab6/myString.java
@@ -2,9 +2,38 @@ package ooplab6;
 
 public class myString {
     public static void main(String[] args) {
-
-
-
+        String s;
+        s = new String();
+        s = "TMUTSV ";
+        System.out.println(s);
+        s = "Welcome " + s;
+        System.out.println(s);
+        s = s.concat(" 2017");
+        System.out.println(s);
+        System.out.println(s.charAt(10));
+        /// comparaion String
+        String s1 = "Hello";
+        String s2 = "Hello";
+        //==
+        if (s1==s2)
+            System.out.println("ข้อความนี้เหมือนกัน");
+        else System.out.println("ข้อความนี้ไม่เหมือนกัน");
+        // equal method
+        if (s1.equals(s2))
+            System.out.println("ข้อความนี้เหมือนกัน");
+        else System.out.println("ข้อความนี้ไม่เหมือนกัน");
+        //compaerto method
+        if (s1.compareTo(s2)==0)
+            System.out.println("ข้อความนี้เหมือนกัน");
+        else if (s1.compareTo(s2)<0) //ค่าที่ได้เป็นบล -
+            System.out.println("ข้อความนี้ไม่เหมือนกัน");
+        else //ค่าที่ได้เป็นบวก +
+            System.out.println("ช้อความทางขวามากกว่า");
+        //trim method
+        String s3 = " Hello  ";
+        System.out.println(s3.length());
+        System.out.println("*"+s3+"*");
+        System.out.println("*"+s3.trim()+"*");
 
     }//main
 
