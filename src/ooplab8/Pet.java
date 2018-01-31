006d07478ff259302dc9bbbeb94b88592fd01103
LAB_COM-MT_1 <sakkarinsuraban@gmail.com>
2018-01-31 10:51:48 +0700

    Pet

---
+++ src/ooplab8/Pet.java
@@ -0,0 +1,42 @@
+package ooplab8;
+
+public abstract class Pet {
+    private String name;
+    private int age;
+//constructor
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    //adstract method
+    public abstract void makeNoise();
+
+    //tostring
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    //setder
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}//class
---
+++ src/ooplab8/PetApp.java
@@ -0,0 +1,11 @@
+package ooplab8;
+
+public class PetApp {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Jame", 2);
+        System.out.println(dog.toString());
+        dog.makeNoise();
+    }
+    //cat
+
+}//class
