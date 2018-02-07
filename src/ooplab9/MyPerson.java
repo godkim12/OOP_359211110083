cacff3002f1d0e2cfaaa0dc07af70eb71e77753f
LAB_COM-MT_1 <sakkarinsuraban@gamil.com>
2018-02-07 10:46:49 +0700

    Person : MyPerson ooplab9

--- src/ooplab9/MyPerson.java
+++ src/ooplab9/MyPerson.java
@@ -2,10 +2,10 @@ package ooplab9;
 
 public class MyPerson {
     public static void main(String[] args) {
-Person person =new Person("111",
-        "k",
-        new Address("1","n","1"),
-        new Job("11",12000));
+Person person =new Person("359211110083",
+        "sakkarin",
+        new Address("758 M.2 ","NakhonSriThammarat","80240"),
+        new Job("Programer",12000));
         System.out.println(person.toString());
 
     }//main
