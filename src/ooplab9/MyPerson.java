c98a8c261cc3a31e984f8e836509d209100958ea
LAB_COM-MT_1 <sakkarinsuraban@gamil.com>
2018-02-07 10:42:36 +0700

    Person : person ooplab9

---
+++ src/ooplab9/MyPerson.java
@@ -0,0 +1,12 @@
+package ooplab9;
+
+public class MyPerson {
+    public static void main(String[] args) {
+Person person =new Person("111",
+        "k",
+        new Address("1","n","1"),
+        new Job("11",12000));
+        System.out.println(person.toString());
+
+    }//main
+}//class
