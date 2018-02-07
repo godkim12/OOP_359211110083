5b593346c2d919361207aaec5b47fc712635f54c
LAB_COM-MT_1 <sakkarinsuraban@gamil.com>
2018-02-07 10:59:21 +0700

    Person : MyPerson edit

--- src/ooplab9/MyPerson.java
+++ src/ooplab9/MyPerson.java
@@ -8,5 +8,7 @@ Person person =new Person("359211110083",
         new Job("Programer",12000));
         System.out.println(person.toString());
 
+        person.getJob().setSalary(40000);
+        System.out.println("Name: "+person.getName()+"Salary: "+person.getJob().getSalary());//เป็นการเปลี่ยนค่าข้องเงิน จากเดิม 12000 เป็น 40000
     }//main
 }//class
